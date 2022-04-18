19105119267a03b3c7142b6c85b8fb2e942e92a1
Ícaro Misquita <icaromisquita@gmail.com>
2022-04-18 17:30:41 +0200

    Update Ex 7.2_count_average.py

--- Ex 7.2_count_average.py
+++ Ex 7.2_count_average.py
@@ -2,14 +2,14 @@
 fname = input("Enter file name: ")
 fh = open(fname)
 count = 0.0
-somatoria = 0.0
+sum = 0.0
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:") : continue
     ini = line.find(" ")
     fim = line.find("\n")
     slice = (line[ini:fim])
     N_slice = float(slice)
-    somatoria = N_slice + somatoria
+    sum = N_slice + somatoria
     count = count + 1
-media = somatoria/count
+media = sum/count
 print("Average spam confidence: " + str(media))
